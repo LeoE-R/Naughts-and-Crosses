740057b915c2e7014bbbe6fce6d7875cf3382183
Leo Ewington-Rodgers <lewingtonrodgers@gmail.com>
2019-10-27 16:57:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Date created
+
+26.10.19
+
+Project Title
+
+Naughts & Crosses
+
+Description
+
+A simple game of naughts and crosses to play against the computer. Player moves first and computer randomly selects moves.
+
+controls - tl: top right, tm: top middle, tr: top right, ml: middle left,
+m: middle, mr: middle right, bl: bottom left, bm: bottom middle, br: bottom right
+
+Once the game comes to an end, a message appears to denote player win, PC win or draw.
+
+Files used
+
+naughts_and_crosses.py
+
+Future Implementations
+
+1) Counter choice; currently there is no choice in which counter the player uses, always 'X'
+2) An option to restart game at any time
+3) Following implementation of a restart option, a score log will be implemented
+4) An option to choose if player or computer goes first; currently the player always makes the first move
---
+++ naughts_and_crosses.py
@@ -0,0 +1,157 @@
+import random
+
+board = ['-','-','-',
+        '-','-','-',
+        '-','-','-']
+
+options = ['tl','tm','tr',
+          'ml', 'm','mr',
+          'bl', 'bm', 'br']
+
+cross = 'X'
+naught = '0'
+
+x = []
+for option in options:
+    if x == []:
+        x.append(option)
+    else:
+        x.append(option)
+
+print('\nxX0o NAUGHTS & CROSSES o0Xx')
+
+print('\nMoves list: tl, tm, tr, ml, m, mr, bl, bm, br\n')
+
+mv_log = []
+
+tkr = 0
+
+def create_board():
+    print('|' + board[0] + '|' + board[1] + '|' + board[2] + '|')
+    print('|' + board[3] + '|' + board[4] + '|' + board[5] + '|')
+    print('|' + board[6] + '|' + board[7] + '|' + board[8] + '|')
+
+def player_move():
+
+    player_move = input('\nYour move!\n')
+
+    while player_move not in x:
+        player_move = input('Invalid move. Select again')
+
+    while player_move in mv_log:
+        player_move = input('Move already made. Select again')
+
+    if player_move == 'tl':
+        board[0] = cross
+    elif player_move == 'tm':
+        board[1] = cross
+    elif player_move == 'tr':
+        board[2] = cross
+    elif player_move == 'ml':
+        board[3] = cross
+    elif player_move == 'm':
+        board[4] = cross
+    elif player_move == 'mr':
+        board[5] = cross
+    elif player_move == 'bl':
+        board[6] = cross
+    elif player_move == 'bm':
+        board[7] = cross
+    else:
+        board[8] = cross
+    mv_log.append(player_move)
+
+def pc_move():
+    print('\nPC move:\n')
+    while True:
+        pc_move = random.choice(x)
+        if pc_move in mv_log:
+            pc_move = random.choice(x)
+        else:
+            mv_log.append(pc_move)
+            break
+
+    if pc_move == 'tl':
+        board[0] = naught
+    elif pc_move == 'tm':
+        board[1] = naught
+    elif pc_move == 'tr':
+        board[2] = naught
+    elif pc_move == 'ml':
+        board[3] = naught
+    elif pc_move == 'm':
+        board[4] = naught
+    elif pc_move == 'mr':
+        board[5] = naught
+    elif pc_move == 'bl':
+        board[6] = naught
+    elif pc_move == 'bm':
+        board[7] = naught
+    else:
+        board[8] = naught
+
+def end_game():
+    global tkr
+
+    if board[0:3] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[3:6] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[6:9] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[2:9:3] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[2:9:2] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[1:8:3] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[2:7:2] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[0:9:4] == ['X', 'X', 'X']:
+        tkr += 1
+    elif board[0:3] == ['0', '0', '0']:
+        tkr += 2
+    elif board[3:6] == ['0', '0', '0']:
+        tkr += 2
+    elif board[6:9] == ['0', '0', '0']:
+        tkr += 2
+    elif board[2:9:3] == ['0', '0', '0']:
+        tkr += 2
+    elif board[2:8:3] == ['0', '0', '0']:
+        tkr += 2
+    elif board[2:7:2] == ['0', '0', '0']:
+        tkr += 2
+    elif board[0:9:4] == ['0', '0', '0']:
+        tkr += 2
+    elif board[2:9:2] == ['0', '0', '0']:
+        tkr += 2
+    elif '-' not in board:
+        tkr = 3
+    else:
+        tkr = 0
+
+def play():
+
+    while '-' in board and tkr == 0:
+        create_board()
+        player_move()
+        end_game()
+        if '-' not in board or tkr != 0:
+            break
+        create_board()
+        pc_move()
+        end_game()
+
+    if tkr == 1:
+        create_board()
+        print('\nYOU WIN! :^)')
+
+    elif tkr == 2:
+        create_board()
+        print('\nPC WINS! :^(')
+
+    else:
+        create_board()
+        print('\nDRAW! :^/')
+
+play()
